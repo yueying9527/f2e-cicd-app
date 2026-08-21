526ecbe1ed8d6f02354b41bbead8bdd132231f94
jinyalong <a1107800582@gmail.com>
2026-08-21 18:44:01 +0800

    fix(deploy): 优化 Node 版本范围并适配 Vercel 部署与 SPA 路由

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/f2e-cicd-app/' : '/',
+  base: './',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
